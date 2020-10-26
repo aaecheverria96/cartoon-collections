946e0232dba3ba0e0fea266d3142d2d1edd28292
Your Username <you@example.com>
2020-10-26 15:04:02 -0400

    commit tree 0fff9731c4d9d7350a57f2c4f23aa9fc1ae15f1d

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) 
+  list_of_dwarves = [] 
+  i = 0 
+  array.each_with_index do |dwarves, index| 
+    list_of_dwarves << (" #{index + 1}. #{dwarves}") 
+    i += 1 
+  end 
+  puts list_of_dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array) 
+  capitalized_calls = [] 
+  i = 0 
+  array.map! do |all| 
+    capitalized_calls << ("#{all}!").capitalize 
+  end 
+  capitalized_calls
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array) 
+  i = 0 
+  array.any? { |i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array) 
+  array.find do |cheese| 
+  cheese_types = ["cheddar", "gouda", "camembert"] 
+  cheese_types.include?(cheese)
 end
+end 
